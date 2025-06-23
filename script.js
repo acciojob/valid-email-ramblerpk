32e8ad70e192e3c8e567f9f1b6476f36834c40ed
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-23 17:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function validEmail(str) {
   //your JS code here.
+	if (str.trim() === '') {
+        return false;
+    }
+
+    // Define the regex pattern for a valid email
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+
+    // Test the string against the regex pattern
+    return emailPattern.test(str);
 }
 
 // Do not change the code below.
